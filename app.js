8341244d6f4defac7f27d2dccf361a8cfa218c61
Faith <pfaithmtan@gmail.com>
2019-08-12 17:46:40 -0700

    Create function to show certain user's timeline
    - Show user's tweets when username clicked
    - Hide all other tweets

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   var $body = $('body');
   var $tweetButton = $('#newTweetButton');
   var $allTweets = $('.allTweets');
+  var $userTweets = $('.userTweets');
 
   var $newTweets = $(":button")
   $newTweets.attr('value', 'See new tweets!');
@@ -12,6 +13,7 @@ $(document).ready(function(){
 
   var showNewTweets = function() {
     $allTweets.show();
+    $userTweets.hide();
 
     var copyOfTweets = streams.home.slice();
     var index = previousLength;
@@ -45,5 +47,32 @@ $(document).ready(function(){
   showNewTweets();
 
   $($newTweets).on('click', showNewTweets);
+
+  var clickUser = function(div) {
+    var user = div.innerHTML.slice(1);
+    $allTweets.hide();
+    $userTweets.show();
+    $userTweets[0].innerHTML = "";
+
+    for (let i = 0; i < streams.users[user].length; i++) {
+      var $tweet1 = $('<div></div>');
+      var $username1 = $('<div></div>');
+      var $tweetBody1 = $('<div></div>');
+      var $time1 = $('<div></div>');
+
+      $tweet1.addClass("tweets");
+      $username1.addClass("user");
+      $tweetBody1.addClass("tweetBod");
+      $time1.addClass("timeStamp");
+      $username1.text('@' + streams.users[user][i].user);
+      $tweetBody1.text(streams.users[user][i].message);
+      $time1.text('Created at: ' + streams.users[user][i].created_at);
+
+      $username1.appendTo($tweet1);
+      $tweetBody1.appendTo($tweet1);
+      $time1.appendTo($tweet1);
+      $tweet1.prependTo($userTweets);
+    }
+  };
 
 });
