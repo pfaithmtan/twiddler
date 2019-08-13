a1edf80791a412e2aadaf150a17f541fedf89552
Faith <pfaithmtan@gmail.com>
2019-08-12 17:22:54 -0700

    Create function to show new tweets
    - Show username and tweet
    - Create button that shows new tweets when clicked
    - Show latest tweets at the top
    - Create separate divs for styling

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 $(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
-    index -= 1;
-  }
+  var $body = $('body');
+  var $tweetButton = $('#newTweetButton');
+  var $allTweets = $('.allTweets');
+
+  var $newTweets = $(":button")
+  $newTweets.attr('value', 'See new tweets!');
+  $newTweets.prependTo($tweetButton);
+
+  var previousLength = 0;
+
+  var showNewTweets = function() {
+    $allTweets.show();
+
+    var copyOfTweets = streams.home.slice();
+    var index = previousLength;
+
+    while(index < copyOfTweets.length){
+      var tweet = copyOfTweets[index];
+      var $tweet = $('<div></div>');
+      var $username = $('<div></div>');
+      var $tweetBody = $('<div></div>');
+
+      $tweet.addClass("tweets");
+      $username.addClass("user");
+      $tweetBody.addClass("tweetBod");
+      $username.text('@' + tweet.user);
+      $tweetBody.text(tweet.message);
+      $username.appendTo($tweet);
+      $tweetBody.appendTo($tweet);
+
+      $tweet.prependTo($allTweets);
+      $($username).on('click', () => clickUser(event.target));
+      index += 1;
+    }
+
+    previousLength = copyOfTweets.length;
+  };
+
+  showNewTweets();
+
+  $($newTweets).on('click', showNewTweets);
 
 });
